0d178ec38f8fc6e301aea1b31aa1f7da6641a0f0
Jim Fowler <jim.fowler2@gmail.com>
2017-07-16 15:14:33 +0000

    Major update

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ f.connect(number_of_pixels);
 var buffer = new Buffer(3*number_of_pixels);
 
 var fillColour = '#FF9329' //warm white at startup;
+f.fill(buffer, fillColour);
+f.push(buffer);
 
 //fillColour = '#000000'; //Off  
 
@@ -16,6 +18,8 @@ var mode = 'solid';
 var ON = true;
 var dirty = true;
 var delay = 100;
+var lambda = 50;
+var j = 0;
 
 var main = function() {
 
@@ -29,6 +33,20 @@ var main = function() {
           f.fill(buffer, fillColour);
           f.push(buffer);
           dirty = false;
+          break;
+        
+        case 'flicker':
+          f.flicker(buffer,fillColour, 255, 200);
+          f.push(buffer);
+          break;        
+        
+        case 'rainbow':
+          f.rainbowFill(buffer, number_of_pixels, j, lambda);
+          f.push(buffer);
+          j++;
+          if (j>lambda) {
+            j=0;
+          };
           break;
 
         default:
@@ -50,10 +68,10 @@ var main = function() {
 
 };
 
-setInterval(main, delay);
+var run = setInterval(main, delay);
 
 
-app.get('/', function(req, res) {
+app.get('/power', function(req, res) {
 
   if(ON) {
     ON = false;
@@ -61,9 +79,61 @@ app.get('/', function(req, res) {
     ON = true;
   }
   dirty = true; 
-
-  console.log('onOff got getted');
+  console.log('ON = '+ON);
+  res.send({"ON": ON});
 });
+
+app.get('/setColour', function(req, res) {
+  if (ON) {
+    fillColour = req.query.colour;
+    dirty = true;
+  }
+  res.send({"COLOUR": fillColour});
+});
+
+
+app.get('/flicker', function(req, res) {
+  if (ON) {
+    if (mode != 'flicker') {
+      mode = 'flicker';
+    } else {
+      mode = 'solid';
+    };
+    dirty = true;
+  };
+  res.send({"MODE": 'flicker'});
+
+});
+
+app.get('/solid', function(req, res) {
+  if (ON) {
+    mode = 'solid';
+    dirty = true;
+    res.send({"MODE": 'solid'});
+  };
+});
+
+app.get('/setDelay', function(req, res) {
+
+  delay = req.query.delay;
+  clearInterval(run);
+  run = setInterval(main, delay);
+  res.send({"DELAY": delay + 'ms'});
+});
+
+app.get('/setLambda', function(req, res) {
+
+  lambda = req.query.lambda;
+  res.send({"LAMBDA": lambda});
+});
+
+app.get('/rainbow', function(req, res) {
+
+  mode = 'rainbow';
+  dirty = true;
+  res.send({"MODE": 'rainbow'});
+});
+
 
 app.listen(3000, function() {
   console.log('listening')
